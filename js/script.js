cc9ded57798ba80ea25f00d94e1403c761db5b98
MattH3w123 <matirz101@gmail.com>
2024-02-15 18:19:03 +0100

    Add new files

--- js/script.js
+++ js/script.js
@@ -52,4 +52,34 @@ SeeMore.onclick = () => {
     copyButton.style.display = 'flex';
   });
 };
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+      } else {
+          entry.target.classList.remove('show');
+      }
+  });
+});
   
+  
+const hiddenEle = document.querySelectorAll('.hidden');
+hiddenEle.forEach((el) => observer.observe(el))
+  
+  
+const Secoundobserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+          entry.target.classList.add('show-fast');
+      } else {
+          entry.target.classList.remove('show-fast');
+      }
+  });
+});
+
+const hiddenElesecend = document.querySelectorAll('.hidden-fast');
+hiddenElesecend.forEach((el) => Secoundobserver.observe(el));
--- js/script3.js
+++ js/script3.js
@@ -174,4 +174,32 @@ SeeMore.onclick = async () => {
 
 
 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+      } else {
+          entry.target.classList.remove('show');
+      }
+  });
+});
   
+  
+const hiddenEle = document.querySelectorAll('.hidden');
+hiddenEle.forEach((el) => observer.observe(el))
+  
+  
+const Secoundobserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+          entry.target.classList.add('show-fast');
+      } else {
+          entry.target.classList.remove('show-fast');
+      }
+  });
+});
+
+const hiddenElesecend = document.querySelectorAll('.hidden-fast');
+hiddenElesecend.forEach((el) => Secoundobserver.observe(el));
